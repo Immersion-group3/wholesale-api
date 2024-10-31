0c285c9dc6bd8d02b85c693671fb9faf5f929974
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-10-31 10:46:50 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+
+
+
+
+// Connect to database
+
+
+
+
+// Create an express app
+const app = express();
+
+
+// Use Middlewares
+
+
+// Use routes
+
+
+
+// Listen for incoming requests
+app.listen(6060, () => {
+    console.log("App is listening on port 6060")
+});
